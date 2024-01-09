ac543a0b4ede38f38aba84018eeb2bbce641aba3
Yogendra Rana <yogendrarana4321@gmail.com>
2024-01-10 00:32:55 +0545

    refactored auth middleware

--- src/middlewares/authMiddleware.go
+++ src/middlewares/authMiddleware.go
@@ -3,7 +3,6 @@ package middlewares
 import (
 	"fmt"
 	"go-gin-auth/src/models"
-	"go-gin-auth/src/services"
 	"net/http"
 	"os"
 	"strings"
@@ -16,21 +15,21 @@ import (
 )
 
 func AuthMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		authHeader := c.GetHeader("Authorization")
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
 		if authHeader == "" {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
 			return
 		}
 
 		authHeaderParts := strings.Split(authHeader, " ")
 		if len(authHeaderParts) != 2 || authHeaderParts[0] != "Bearer" {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid Authorization header format"})
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid authorization header format"})
 			return
 		}
 
 		accessToken := authHeaderParts[1]
-		isValidAccessToken, userIDPtr := checkAccessTokenValidity(accessToken, c)
+		isValidAccessToken, userIDPtr := validateAccessToken(accessToken, ctx)
 
 		// if isValidAccessToken=false and userIDPtr=nil, then terminate the request
 		if !isValidAccessToken && userIDPtr == nil {
@@ -38,63 +37,24 @@ func AuthMiddleware() gin.HandlerFunc {
 		}
 
 		// find the user in the database
-		db := c.MustGet("db").(*gorm.DB)
+		db := ctx.MustGet("db").(*gorm.DB)
 		userID := *userIDPtr
 		user, err := models.GetUserByID(db, userID)
 
 		if err != nil {
-			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve user"})
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve user"})
 			return
 		}
 
 		if isValidAccessToken {
-			c.Set("user", user)
-			c.Next()
-		}
-
-		if !isValidAccessToken {
-			refreshToken, err := c.Cookie("refresh_token")
-			if err != nil {
-				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Refresh token not found"})
-				return
-			}
-
-			// check validity of refresh token
-			isValidRefreshToken, err := checkRefreshTokenValidity(refreshToken, user.RefreshTokens)
-			if err != nil {
-				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Failed to compare refresh token hashes"})
-				return
-			}
-
-			// if valid refresh token issue new access token
-			if isValidRefreshToken {
-				// generate access tokens
-				accessSignedToken, err := services.GenerateAccessToken(user.ID)
-				if err != nil {
-					c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate token"})
-					return
-				}
-
-				c.JSON(http.StatusOK, gin.H{"new_access_token": accessSignedToken})
-
-				// set the user in the Gin context
-				c.Set("user", user)
-
-				// Call c.Next() to pass the request to the next handler
-				c.Next()
-			}
-
-			// if invalid refresh token terminate the request
-			if !isValidRefreshToken {
-				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid refresh token"})
-				return
-			}
+			ctx.Set("user", user)
+			ctx.Next()
 		}
 	}
 }
 
 // check validity of access token
-func checkAccessTokenValidity(accessToken string, c *gin.Context) (bool, *uint) {
+func validateAccessToken(accessToken string, c *gin.Context) (bool, *uint) {
 	token, err := jwt.Parse(accessToken, func(token *jwt.Token) (interface{}, error) {
 		// not required to check signing method but a good practice
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
@@ -130,7 +90,7 @@ func checkAccessTokenValidity(accessToken string, c *gin.Context) (bool, *uint) 
 }
 
 // check validity of refresh token
-func checkRefreshTokenValidity(refreshToken string, hashedTokens []models.RefreshToken) (bool, error) {
+func validateRefreshToken(refreshToken string, hashedTokens []models.RefreshToken) (bool, error) {
 	err := bcrypt.CompareHashAndPassword([]byte(hashedTokens[0].TokenHash), []byte(refreshToken))
 	if err != nil {
 		return false, err
